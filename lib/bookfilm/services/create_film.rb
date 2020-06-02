b154b3e53084a2f5f760fef4289303b094689339
Daniel Gomez-Sierra <danielgomezsierra@gmail.com>
2020-06-02 10:41:57 -0500

    Remove unnecessary validation when creating a film

--- lib/bookfilm/services/create_film.rb
+++ lib/bookfilm/services/create_film.rb
@@ -4,19 +4,9 @@ module Bookfilm
   class CreateFilm
     include Dry::Transaction
 
-    ERRORS = []
-    VALIDATION = {
-      required: [:name, :description, :image_url, :rolling_days]
-    }
-
-    step :validate
     step :create
 
     private
-
-    def validate(params)
-      Success(params)
-    end
 
     def create(params)
       film = Film.create(params)
